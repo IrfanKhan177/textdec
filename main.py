4335869702c24cf59ec789f49b8bc83e21670524
Irfan Khan <irfankhan465c@gmail.com>
2022-02-23 21:34:15 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template ,request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    
+    param ={'title':'Home'}
+    return render_template('index.html',param=param)
+
+@app.route("/analyzed")
+def analyzed():
+  text = request.args.get('text','default')
+  rem = request.args.get('rem','off')
+  if rem == "on":
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    ana =""
+    for char in text:
+      if char not in punctuations:
+        ana = ana + char
+    param = {
+      'hed':'Analyzed text',
+      'antext':ana,'title':'Without Punctuations',
+    }
+  
+    return render_template('ana.html',param=param)
+  else:
+    return "Choose any option."
+    
+app.run(debug=False)
