e38ee4ea220662f9702f63c521a0f3e87a1cf0ca
Irfan Khan <93929266+IrfanKhan177@users.noreply.github.com>
2022-02-26 14:14:18 +0600

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,17 @@ def analyzed():
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404  
+@app.after_request
+
+def after_request(response):
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+
+    return response
 if __name__=="__main__":
     app.run(port="5000")
     
