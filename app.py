8200c1ce91a6c588e8a92781c2c0f72dc8e0dd61
Irfan Khan <93929266+IrfanKhan177@users.noreply.github.com>
2022-02-26 14:07:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,6 @@ def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404  
 if __name__=="__main__":
-    app.run(debug=False)
+    app.run(port="5000")
     
         
